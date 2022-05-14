563627d1f8570a0f84598e471c728155ee209e6d
hczzl <362349050@qq.com>
2022-05-14 16:24:37 +0800

    spring cloud框架

---
+++ study-model01/src/test/java/com/zzl/studymodel01/StudyModel01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzl.studymodel01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyModel01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
